233dbc620b7dcb17503f1bbb9e38caa4478f2ce5
Alex <ahawknj15@gmail.com>
2022-02-13 16:18:19 -0500

    style: Add link styling on gifs
    Added a link icon that appears on hover that directs users to the Giphy site to interact further with the gif. On desktop it appears on hover and is accessible. On mobile it appears in the lefthand corner of each gif. Styling on mobile should be designed better, but tabling for now.

--- src/components/ApiContainer.vue
+++ src/components/ApiContainer.vue
@@ -14,13 +14,18 @@
     <!-- gif container -->
     <div class="gif-container">
       <!-- gif cards displayed using v-for directive -->
-      <div class="gif-card" v-for="gif in gifs" :key="gif.id">
+      <div class="gif-card" v-for="gif in gifs" :key="gif.id" tabindex="0">
         <img 
           :src="gif.images.fixed_width.url" 
           :alt="gif.title"
         />
-        <p class="share"><a href="{{gif.url}}">Share</a></p>
+        <div class="overlay">
+          <a :href="gif.bitly_gif_url" target="_blank" aria-role="button" tabindex="0"><svg 
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M172.5 131.1C228.1 75.51 320.5 75.51 376.1 131.1C426.1 181.1 433.5 260.8 392.4 318.3L391.3 319.9C381 334.2 361 337.6 346.7 327.3C332.3 317 328.9 297 339.2 282.7L340.3 281.1C363.2 249 359.6 205.1 331.7 177.2C300.3 145.8 249.2 145.8 217.7 177.2L105.5 289.5C73.99 320.1 73.99 372 105.5 403.5C133.3 431.4 177.3 435 209.3 412.1L210.9 410.1C225.3 400.7 245.3 404 255.5 418.4C265.8 432.8 262.5 452.8 248.1 463.1L246.5 464.2C188.1 505.3 110.2 498.7 60.21 448.8C3.741 392.3 3.741 300.7 60.21 244.3L172.5 131.1zM467.5 380C411 436.5 319.5 436.5 263 380C213 330 206.5 251.2 247.6 193.7L248.7 192.1C258.1 177.8 278.1 174.4 293.3 184.7C307.7 194.1 311.1 214.1 300.8 229.3L299.7 230.9C276.8 262.1 280.4 306.9 308.3 334.8C339.7 366.2 390.8 366.2 422.3 334.8L534.5 222.5C566 191 566 139.1 534.5 108.5C506.7 80.63 462.7 76.99 430.7 99.9L429.1 101C414.7 111.3 394.7 107.1 384.5 93.58C374.2 79.2 377.5 59.21 391.9 48.94L393.5 47.82C451 6.731 529.8 13.25 579.8 63.24C636.3 119.7 636.3 211.3 579.8 267.7L467.5 380z"/>
+          </svg></a> 
+        </div>
       </div>
+
     </div> 
   </section>
 </template>
@@ -61,7 +66,7 @@
                 this.apiErrorMsg = "Nothing Found, Please enter your search in the box above";
               } 
               // console.log("META", response.data.meta);
-              // console.log(response.data.data);
+              console.log(response.data.data);
             })
             .catch((error) => {
               console.log("error", error);
@@ -115,6 +120,7 @@
       padding: 11px 10px 9px;
       border: 1px solid $red;
       width: 50%;
+      line-height: 1rem;
 
       @media (min-width: $tablet) {
         width: 40%;
@@ -125,10 +131,11 @@
     .gif-container {
       column-count: 2;
       margin: 40px auto;
-      padding: 20px;
+      padding: 0 8px;
       @media (min-width: $tablet){ 
         column-count: 3;
         max-width: 650px;
+        padding: 20px;
       }
       @media (min-width: $desktop) {
         column-count: 3;
@@ -139,49 +146,60 @@
         display: grid;
         grid-template-rows: 1fr auto;
         break-inside: avoid; /* IE 10+ */
-        gap: 10px;
         -webkit-column-break-inside: avoid; /* Chrome, Safari, Opera */
         page-break-inside: avoid; /* Firefox */
         padding: 10px 5px;
         position: relative;
+        
+        @media (min-width: $desktop) {
+          gap: 10px;
+          &:not(:hover, :focus, :active, :focus-visible, :focus-within) {
+            .overlay {
+              visibility: hidden;
+            }
+          }     
+        }
 
         img {
           width: 100%;
           display: block;
-          border-radius: 7px;
+          @media (min-width: $tablet) {
+            border-radius: 7px;
+          }
         }
 
-        .share {
-          display: none;
+        .overlay {
           position: absolute;
-          bottom: 15px;
-          left: 50%;
-          transform: translate(-50%);
-          background: $red;
-          padding: 5px 15px;
-          &:hover {
-            background: rgba($red, 0.8);
-          }
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 10%;
+          z-index: 2;
+          text-align: left;
 
-          a {
-            color: $cream;
-            text-decoration: none;
-            text-transform: uppercase;
-            letter-spacing: 0.5px;
-            font-style: italic;
-          }
+          svg {
+            fill: $cream;
+            width: 20px;
+            height: 20px;
+            background: $red;
+            border-radius: 50%;
+            padding: 5px;
+            @media (min-width: $tablet) {
+              margin: 15px 5px;
+            }
 
+            &:hover {
+              background: $purple;
+            }
+          }
         }
 
-        &:hover {
+        &:hover, &:focus {
           @media (min-width: $desktop) {
             img {
               transition: 0.15s all;
               box-shadow: 2px 2px 7px 0px rgba($purple, 0.4);
               transform: scale(1.1);
-            }
-            .share {
-              display: block;
             }
           }
         }
